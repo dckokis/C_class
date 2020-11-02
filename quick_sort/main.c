000d20b0f8446b7f0bb924c90066320de4a60991
Konstantin Berengolts <63435062+dckokis@users.noreply.github.com>
2020-11-02 16:17:04 +0700

    Программа уходит в бесконечный цикл

--- quick_sort/main.c
+++ quick_sort/main.c
@@ -41,39 +41,56 @@ void swap(void* a, void* b, const size_t size) {
 // в зависимости от используемого типа данных в массиве
 void quick_sort(void* arr, unsigned int len_arr,
                 unsigned int size_elem, int (*cmp)(void*, void*)) {
+    if(len_arr == 1){
+        return;
+    }
     unsigned int i = 0; // индексы левой половины
     unsigned int j = len_arr - 1; // индексы правой половины
-    void *median = (char *)arr + (len_arr / 2) * size_elem;
+    void *median = (char *)arr + (len_arr/ 2) * size_elem;
     void *left = (char *)arr;
     void *right = (char *)arr + (len_arr - 1) * size_elem;
-    while (i != j && i < len_arr - 1 && j > 1) {
-        if (cmp(median, left) < cmp(median, right)) {
+    while (i < j && i < len_arr - 1 && j > 0) {
+        if (cmp(median, left) == 1 && cmp(median, right) == -1) {
             i++;
             j--;
             continue;
-        } else if (cmp(median, left) > cmp(median, right)) {
+        } else if (cmp(median, left) == -1 && cmp(median, right) == 1) {
             swap(left, right, size_elem);
             i++;
             j--;
         } else if (cmp(median, left) == cmp(median, right)) {
-            i++;
-            j--;
+            if(cmp(left, right) == 1){
+                j--;
+                continue;
+            }else if(cmp(left, right) == -1){
+                i++;
+                continue;
+            }else if(cmp(left, right) == 0){
+                if(cmp(median, left) == 1){
+                    i++;
+                    continue;
+                }else if(cmp(median, left) == -1){
+                    j--;
+                    continue;
+                }
+            }
             continue;
         }
     }
+
+    swap(median, right, size_elem);
     unsigned int size_left = len_arr / 2;
     unsigned int size_right = len_arr - size_left;
-    while (left != right){
-        quick_sort((char *)arr, size_left, size_elem, cmp);
-        quick_sort((char *)arr + size_right * size_elem, size_right, size_elem, cmp);
-    }
+    quick_sort((char *)arr, size_left, size_elem, cmp);
+    quick_sort((char *)arr + size_right * size_elem, size_right, size_elem, cmp);
 }
 int main() {
     double A[N] = {0};
     FILE *stream;
-    stream = fopen ("Input.txt", "r");
+    stream = fopen ("C:\\C_class\\quick_sort\\cmake-build-debug\\CMakeFiles\\Input.txt", "r");
     if(stream == 0){
         printf("File is not found");
+        exit(1);
     }
     int i;
     unsigned int length;
@@ -81,12 +98,17 @@ int main() {
     unsigned  int amount  = 0;
     for(i = 0; i < length; i++) {
         fscanf_s(stream, "%lf", &A[i]);
+        printf("%g ", A[i]);
         amount ++;
     }
     if(amount != length){
-        exit(0);
+        exit(1);
     }
-    size_t size_elem = sizeof(A[0]);
+    size_t size_elem = sizeof(double);
     quick_sort(A, length, size_elem, double_cmp);
+    printf("Sorted array:");
+    for(i = 0; i < length; i++){
+        printf("%g ", A[i]);
+    }
     return 0;
 }
