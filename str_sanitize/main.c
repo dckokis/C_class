6bf9ef32376278f9b4852696226c4e93466bcd4f
Konstantin Berengolts <63435062+dckokis@users.noreply.github.com>
2020-11-08 18:46:57 +0700

    Unfinished.

--- str_sanitize/main.c
+++ str_sanitize/main.c
@@ -3,25 +3,35 @@
 #define N 1000
 // пунктуацию дабавить, новое предложение с большой буквы, убирать пробелы
 // в начале и в конце строки
-void str_sanitize(char *string)
-{
+void str_sanitize(char *string){
     int r, w;
     int space = 0;
+    int point = 0;
+
     for (r = w = 0; string[r] != '\0'; r++) {
         if (isspace(string[r]) == 0){
+            if (ispunct(string[r]) != 0) {
+                if (string[r] == ".") {
+                    point = 1;
+                    if (space == 1) {
+                        string[w] = string[r];
+                    }
+                    
+                } else
+            }
             string[w++] = string[r];
             space = 0;
-        }else{
-            if(space == 0 && w != 0){
+        } else{
+            if (space == 0 && w != 0){
                 string[w++] = string[r];
             }
-            if(w == 0){}
+            if (w == 0){}
             space = 1;
         }
     }
-    if(isspace(string[w - 1] != 0)){
+    if (isspace(string[w - 1]) != 0){
         string[w - 1] = '\0';
-    }else {
+    } else {
         string[w] = '\0';
     }
 }
