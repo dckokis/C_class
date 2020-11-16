552f32de544891be7f56f484c8abb517c2e2a0d6
Konstantin Berengolts <63435062+dckokis@users.noreply.github.com>
2020-11-16 16:18:11 +0700

    Переписал сортировку с memcpy.

--- advanced_qck_srt/quick_sort.c
+++ advanced_qck_srt/quick_sort.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "quick_sort.h"
 
-// написать свой memcpy и вызывать его в swap. Корректно обрабатывать ситуацию когда одна часть памяти нахлестывается на другую
-// ситуации когда dest раньше source и наоборот
-// создать структуру с множеством полей и сортировать ее по конкретному полю(написать разные компараторы int/double лексикографический), читать данные из файла в структуру
-// сделать функцию которая из FILE * прочитает структуру
 int int_cmp(void *x, void *y){
     return *(int*)x - *(int*) y;
 }
@@ -14,13 +12,11 @@ double double_cmp(void *x, void *y) {
 }
 
 void swap(void *a, void *b, const size_t size) { // сделать с memcpy
-    for (size_t i = 0; i < size; i++) {
-        char *i1 = (char *) a + i;
-        char *i2 = (char *) b + i;
-        char tmp = *i1;
-        *i1 = *i2;
-        *i2 = tmp;
-    }
+    void * tmp = malloc(size);
+    memcpy(tmp, a, size);
+    memcpy(a, b, size);
+    memcpy(b, tmp, size);
+    free(tmp);
 }
 
 void quick_sort(void *arr, unsigned int len_arr,
@@ -47,4 +43,7 @@ void quick_sort(void *arr, unsigned int len_arr,
     // recursion
     if (len_arr > i) quick_sort((char *) arr + i * size_elem, median, size_elem, cmp); //right
     if (j > 0) quick_sort((char *) arr, median + 1, size_elem, cmp); // left
+}
+int main(){
+    return 0;
 }
