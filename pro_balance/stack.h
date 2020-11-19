1abfda8323c0323d547d73f9d548c3cb239fda0c
Konstantin Berengolts <63435062+dckokis@users.noreply.github.com>
2020-11-19 17:39:54 +0700

    начал делать

---
+++ pro_balance/stack.h
@@ -0,0 +1,11 @@
+#ifndef STACK_STACK_H
+#define STACK_STACK_H
+typedef void *Pointer;
+typedef struct Stack;
+void stack_create(Stack *s);
+void stack_destroy(Stack *s);
+size_t stack_size(Stack *s);
+void stack_push(Stack *s, void *elem);
+Pointer stack_pop(Stack *s);
+Pointer stack_peek(Stack *s);
+#endif
