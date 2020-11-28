e8e0b34e3616af289e64e6b100ebf710727f0bb9
Konstantin Berengolts <63435062+dckokis@users.noreply.github.com>
2020-11-28 23:22:44 +0700

    Реализовал алгоритм, который проверяет балланс скобок не только в скобочном выражении, но и в обычном предложении/тексте со скобками

--- pro_balance/main.c
+++ pro_balance/main.c
@@ -6,23 +6,26 @@
 
 
 ///* Функция возвращает 1, если скобочное выражение корректно, или 0 в противном случае. *///
-// кладем открывающую скобку на стек, смотрим закрывающую, снимаем элемент, смотрим парные или нет
-// (()[]{>
-// в конце смотрим пустой ли стек
 int check_brackets(char *expression, size_t lenght) {
     char *open_bracket = "([{<";
     char *closed_bracket = ")]}>";
 
     Stack brackets_stack;
     stack_create(&brackets_stack);
+
     for (int i = 0; i < lenght; ++i) {
         for(int j = 0; j < 4; j++) {
-            if ((expression + i) == (open_bracket + j)){
+            if (*(expression) == *(closed_bracket + j)) { // if expression starts with open bracket it is incorrect
+                stack_destroy(&brackets_stack);
+                return 0;
+            } else if (*(expression + i) == *(open_bracket + j)){ // if current char is open bracket push it to stack
                 stack_push(&brackets_stack, expression + i);
-            } else if ((expression + i) == (closed_bracket + j)) {
-                if (stack_peek(&brackets_stack) == open_bracket + j) {
+                break;
+            } else if (*(expression + i) == *(closed_bracket + j)) {
+                if (*(char *)stack_peek(&brackets_stack) == *(open_bracket + j)) { // if the last open bracket is paired with current closed bracket pop it from stack peek
                     stack_pop(&brackets_stack);
-                } else break;
+                    break;
+                }
             }
         }
     }
@@ -36,7 +39,7 @@ int check_brackets(char *expression, size_t lenght) {
 
 }
 int main() {
-    char *expression = malloc(100);
+    char *expression = malloc(N);
     gets_s(expression, N);
     size_t len = strlen(expression);
     printf("%d\n", check_brackets(expression, len));
