a8e4495ff488055f9da2419a4bab7b3613b1e096
Konstantin Berengolts <63435062+dckokis@users.noreply.github.com>
2020-11-28 20:12:45 +0700

    Реализовал алгоритм, но не могу понять, правильно ли я доступаюсь до элементов, корректно ли использовать expression + i  и тд. Если запустить дебагер, то видно, что if и else if он просто проскакивает, хотя, казалось бы, не должен

--- pro_balance/main.c
+++ pro_balance/main.c
@@ -15,37 +15,29 @@ int check_brackets(char *expression, size_t lenght) {
 
     Stack brackets_stack;
     stack_create(&brackets_stack);
-    // sizeof(char) можно убрать
     for (int i = 0; i < lenght; ++i) {
         for(int j = 0; j < 4; j++) {
-            if (expression + i * sizeof(char) == open_bracket + j * sizeof(char) || expression + i * sizeof(char) == closed_bracket + j * sizeof(char)){
-                stack_push(&brackets_stack, expression + i * sizeof(char));
+            if ((expression + i) == (open_bracket + j)){
+                stack_push(&brackets_stack, expression + i);
+            } else if ((expression + i) == (closed_bracket + j)) {
+                if (stack_peek(&brackets_stack) == open_bracket + j) {
+                    stack_pop(&brackets_stack);
+                } else break;
             }
         }
     }
-    for(int j = 0; j < 4; j++) {
-        if (stack_peek(&brackets_stack) == open_bracket + j * sizeof(char)){
-            return 0;
-        }
+    if (stack_size(&brackets_stack) == 0){
+        stack_destroy(&brackets_stack);
+        return 1;
+    } else {
+        stack_destroy(&brackets_stack);
+        return 0;
     }
-    int flag[4] = {0}; // 1 ) 2 ] 3 } 4 >
-    for (int i = 0; i < stack_size(&brackets_stack); i++) {
-        for(int j = 0; j < 4; j++) {
-            if (stack_pop(&brackets_stack) == closed_bracket + j * sizeof(char)) {
-                flag[j]++;
-                stack_pop(&brackets_stack);
-            } else if (stack_pop(&brackets_stack) == open_bracket + j * sizeof(char)) {
-                flag[j]--;
-                stack_pop(&brackets_stack);
-            }
-        }
-    }
-    if (flag[0] == 0 && flag[1] == 0 && flag[2] == 0 && flag[3] == 0) return 1;
-    else return 0;
+
 }
 int main() {
     char *expression = malloc(100);
-    scanf("%s", expression);
+    gets_s(expression, N);
     size_t len = strlen(expression);
     printf("%d\n", check_brackets(expression, len));
     return 0;
